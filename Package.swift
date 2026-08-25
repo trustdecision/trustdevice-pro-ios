53428b8e05124831cbcd3a840c86112ccbcd8e67
zeinber <zhanbin.cheng@tongdun.net>
2026-08-25 15:29:06 +0800

    Release 5.3.1-7

--- Package.swift
+++ Package.swift
@@ -28,18 +28,18 @@ let package = Package(
         ),
         .binaryTarget(
             name: "TDMobRisk",
-            url: "https://static.trustdecision.com/fp-ios/5.3.1/spm/TDMobRisk_xcframework_v5.3.1.5_8c50797.zip",
-            checksum: "c7bcbfb251c9251abecf78d371df053bda1b3a10134df5f7dcc8fffb1d0cefb3"
+            url: "https://static.trustdecision.com/fp-ios/5.3.1/spm/TDMobRisk_xcframework_v5.3.1.6_a1a9fca_2.zip",
+            checksum: "8f277d1d4572d21b93357a242c584495a0fe66ee1638512174cbeae7e113294f"
         ),
         .binaryTarget(
             name: "TDCorePlugin",
-            url: "https://static.trustdecision.com/fp-ios/5.3.1/spm/TDCorePlugin_xcframework_v4.3.6_af8c025.zip",
-            checksum: "6aab09ad7b8e66a7314162b1744b18ec5e632d59181ece6132516ab7bbb76a74"
+            url: "https://static.trustdecision.com/fp-ios/5.3.1/spm/TDCorePlugin_xcframework_v4.3.7_9ddec2b_2.zip",
+            checksum: "8101f545cee5e7915c81f8b7f11a97d6ae977318a04827b32cf4996680cf14f7"
         ),
         .binaryTarget(
             name: "FMDeviceManagerFramework",
-            url: "https://static.trustdecision.com/fp-ios/5.3.1/spm/FMDeviceManagerFramework_xcframework_v5.3.1.5_677d88f.zip",
-            checksum: "a1c79ce2a5bfb2b552c8c739b583e2be5f81d5289194b8ae83d914ba9e980f2d"
+            url: "https://static.trustdecision.com/fp-ios/5.3.1/spm/FMDeviceManagerFramework_xcframework_v5.3.1.6_3847808_2.zip",
+            checksum: "02ff2d551ef239425874f440469e3b61fa3cdbba5f3b1978db36ac03ce36156b"
         )
     ]
 )
